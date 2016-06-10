768b3c3c55b9aa4a814f8f2f41ecefbea14a1f8d
Muhammad Usman <muhammad.usman@widespace.com>
2016-06-10 16:32:01 +0200

    Fixed Build process

--- gulpfile.js
+++ gulpfile.js
@@ -60,33 +60,8 @@ gulp.task('css', function() {
 **/
 gulp.task('watch', function() {
   gulp.watch('less/*.*', ['css']);
-<<<<<<< Updated upstream
-  gulp.watch('js/*.*', ['babelfy', browserSync.reload]);
-});
-
-
-/**
-* BrowserSync setup to reloads app page
-* each time browserSync.reload called in watch
-* NOT IN USE
-* if you want to use please run 'browserSync' before watch
-* i.e. gulp.task('watch', ['browserSync'], function() {
-* and browserSync.reload after css and babelfy
-* i.e gulp.watch('less/*.*', ['css', browserSync.reload]);
-**/
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      proxy: "localhost:9800"
-    }
-  });
-});
-
-gulp.task('default', ['babelfy', 'css', 'watch']);
-gulp.task('build', ['babelfy', 'css', 'watch']);
-=======
   gulp.watch('js/*.*', ['babelfy']);
 });
 
+
 gulp.task('default', ['babelfy', 'browserify', 'css', 'watch']);
->>>>>>> Stashed changes
